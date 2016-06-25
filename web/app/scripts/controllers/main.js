969862debf502e0bc1e8c4e20ef459930eedd03c
Louis Houghton <louis@louishoughton.com>
2016-06-25 18:59:41 +0100

    Add map marker for users current position

--- web/app/scripts/controllers/main.js
+++ web/app/scripts/controllers/main.js
@@ -8,10 +8,33 @@
  * Controller of the webApp
  */
 angular.module('webApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('MainCtrl', ['$scope', 'leafletData', function ($scope, leafletData) {
+
+      $scope.findMyRoute = function() {
+          console.log($scope.options);
+      }
+
+      angular.extend($scope, {
+          center: {
+              autoDiscover: true,
+              zoom: 12
+          }
+      });
+
+      leafletData.getMap('map').then(function(map) {
+          map.locate({setView: true, maxZoom: 16, watch: true, enableHighAccuracy: true});
+          map.on('locationfound', function (e) {
+              console.log(e.latlng, e.accuracy)
+              angular.extend($scope, {
+                  markers: {
+                      me: {
+                          lat: e.latlng.lat,
+                          lng: e.latlng.lng
+                      }
+                  }
+              });
+          });
+      });
+
+
+  }]);
