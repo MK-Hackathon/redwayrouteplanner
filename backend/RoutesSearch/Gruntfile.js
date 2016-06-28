ec20d02dee06070da477ffc64e6ae48716bd58df
Yijun Yu <yijun.yu@gmail.com>
2016-06-28 12:14:20 +0100

    follows the instructions on how to deploy lambda functions on CLI

---
+++ backend/RoutesSearch/Gruntfile.js
@@ -0,0 +1,50 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'src/<%= pkg.name %>.js',
+        dest: 'build/<%= pkg.name %>.min.js'
+      }
+    },
+    lambda_invoke: {
+        default: {
+            options: {
+                // Task-specific options go here.
+            }
+        }
+    },
+    lambda_package: {
+        default: {
+            options: {
+                // Task-specific options go here.
+            }
+        }
+    },
+    lambda_deploy: {
+        default: {
+	    arn: 'arn:aws:lambda:eu-west-1:303107930060:function:RoutesSearch',
+            options: {
+                // Task-specific options go here.
+                region : "eu-west-1"
+            }
+        }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  // grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Default task(s).
+  // grunt.registerTask('default', ['uglify']);
+
+  grunt.loadNpmTasks('grunt-aws-lambda');
+
+  grunt.registerTask('deploy', ['lambda_package', 'lambda_deploy:default']);
+
+};
