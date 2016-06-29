1e54421c8c9566d3929a676221d38b7ebceeb64c
Yijun Yu <yijun.yu@gmail.com>
2016-06-29 11:52:12 +0100

    using Neil's backend update approach to revise the Lambda functions; also refactored the RoutesSearch function to avoid duplicating the code

--- backend/lambda/c.sh
+++
@@ -1,3 +0,0 @@
-curl -X POST -d "{\"Start\":\"Willen Lake\",\"End\":\"Walnut Tree\"}" https://3ffpwfgm6h.execute-api.eu-west-1.amazonaws.com/prod/routes
-#curl -X POST -d "{\"Start\":\"Willen Lake\",\"End\":\"Walnut Tree\"}" https://qu9w39owyb.execute-api.eu-west-1.amazonaws.com/prod/RoutesTest
-
