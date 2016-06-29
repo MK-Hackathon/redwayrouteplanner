94a7ea2f143c6d704065f474fa1254ca2769bbd8
Neil Smith <neil.git@njae.me.uk>
2016-06-29 13:45:12 +0100

    Restructured AWS lambda function done.

--- backend/GruntTest/index.js
+++ backend/GruntTest/index.js
@@ -1,18 +1,8 @@
 'use strict';
 const https = require('https');
 const async = require('async');
-const waterfall = require('async-waterfall');
 
-console.log('Loading function');
-var toReturn = '';
-var resultsSoFar = [];
-
-
-function readChunk (chunk, responseString) {
-    responseString += chunk.toString('utf8');
-};
-
-function getLocation(location, responseString) {
+function getLocation(location, callback) {
     const options = {
         host: "nominatim.openstreetmap.org",
         port: 443,
@@ -20,8 +10,8 @@ function getLocation(location, responseString) {
         method: 'GET',
         json:true
     };
-    responseString = '';
 
+    var responseString = '';
     https.request(options,
         function(res) {
             res.on('data', (chunk) => {
@@ -29,29 +19,49 @@ function getLocation(location, responseString) {
             });
 
             res.on('end', () => {
-                responseString += "\nFinished reading";
+                var response = JSON.parse(responseString);
+                var latLong = encodeURI(response[0].lat + ',' + response[0].lon);
+                callback(null, latLong);
             });
         }
     ).end();
 };
 
+function getRoute(locations, options, callback) {
+    var requestOptions = {
+        host: "graphhopper.com",
+        port: 443,
+        path: '/api/1/route?point=' + locations[0] + '&point=' + locations[1] + '&vehicle=bike&key=a55445eb-e961-4151-8dce-e818b0052d75',
+        method: 'GET',
+        json:true
+    };
+
+    var responseString = '';
+    https.request(requestOptions,
+        function(res) {
+            res.on('data', (chunk) => {
+                responseString += chunk.toString('utf8');
+            });
+
+            res.on('end', () => {
+                var route = responseString.toString('utf8');
+                callback(null, route);
+            });
+        }
+    ).end();
+};
+
+
 exports.handler = (event, context, lambdaCallback) => {
 
-    var locationResults;
-    var startLocation;
-    var startLocationString;
+    var err;
+    var results;
 
-    waterfall([
-        function (finalStep){
-            finalStep(null, toReturn);
-        }
-        getLocation(event.Start, startLocationString)
-        ], 
-        lambdaCallback(null, toReturn));
-
-    getLocation(event.Start, startLocationString);
-
-    toReturn = startLocationString;
-
-    lambdaCallback(null, toReturn);
+    async.parallel([
+        function(callback) {getLocation(event.Start, callback)},
+        function(callback) {getLocation(event.End, callback)}
+        ],
+        function (err, results) {
+            getRoute(results, {}, lambdaCallback);
+        });
 };
