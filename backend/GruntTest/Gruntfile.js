e053ded0371e92d9a36a3b0168542d11fccb4386
Neil Smith <neil.git@njae.me.uk>
2016-06-30 09:17:44 +0100

    Added deployment stages

--- backend/GruntTest/Gruntfile.js
+++ backend/GruntTest/Gruntfile.js
@@ -1,50 +1,69 @@
 module.exports = function(grunt) {
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+          options: {
+            banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: 'src/<%= pkg.name %>.js',
+                dest: 'build/<%= pkg.name %>.min.js'
+            }
+        },
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
-      }
-    },
-    lambda_invoke: {
-        default: {
-            options: {
+        lambda_invoke: {
+            default: {
+                options: {
                 // Task-specific options go here.
+                }
+            }
+        },
+
+        lambda_package: {
+            default: {
+              options: {
+                // Task-specific options go here.
+                }
+            },
+            prod: {
+                options: {
+                // Task-specific options go here.
+                }
+            }
+        },
+
+        lambda_deploy: {
+            default: {
+                arn: 'arn:aws:lambda:eu-west-1:303107930060:function:GruntTest',
+                options: {
+                    // Task-specific options go here.
+                    region : "eu-west-1",
+                    aliases: "beta",
+                    enableVersioning: true
+                }
+            },
+            prod: {
+                arn: 'arn:aws:lambda:eu-west-1:303107930060:function:GruntTest',
+                options: {
+                    // Task-specific options go here.
+                    region : "eu-west-1",
+                    aliases: "prod",
+                    enableVersioning: true
+                }
             }
         }
-    },
-    lambda_package: {
-        default: {
-            options: {
-                // Task-specific options go here.
-            }
-        }
-    },
-    lambda_deploy: {
-        default: {
-            arn: 'arn:aws:lambda:eu-west-1:303107930060:function:GruntTest',
-            options: {
-                // Task-specific options go here.
-                region : "eu-west-1"
-            }
-        }
-    }
-  });
+    });
 
-  // Load the plugin that provides the "uglify" task.
-  // grunt.loadNpmTasks('grunt-contrib-uglify');
+    // Load the plugin that provides the "uglify" task.
+    // grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  // Default task(s).
-  // grunt.registerTask('default', ['uglify']);
+    // Default task(s).
+    // grunt.registerTask('default', ['uglify']);
 
-  grunt.loadNpmTasks('grunt-aws-lambda');
+    grunt.loadNpmTasks('grunt-aws-lambda');
 
-  grunt.registerTask('deploy', ['lambda_package', 'lambda_deploy:default']);
+    grunt.registerTask('deploy', ['lambda_package', 'lambda_deploy:default']);
+    grunt.registerTask('deploy_prod', ['lambda_package', 'lambda_deploy:prod']);
 
 };
