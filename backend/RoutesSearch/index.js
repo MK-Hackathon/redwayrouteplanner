f685e6533944f663b04798b5866e46d93afaae3d
Neil Smith <neil.git@njae.me.uk>
2016-07-12 18:32:32 +0100

    Removed backend/GruntTest

--- backend/RoutesSearch/index.js
+++ backend/RoutesSearch/index.js
@@ -1,73 +1,74 @@
-'use strict';
-var https = require('https');
-// var async = require('async');
-// var query = {in: ["Willen Lake", "Walnut Tree"], out: ""};
-// var routesSearch = async.compose(graphHopper, openStreetMap, openStreetMap);
+"use strict";
+const https = require("https");
+const async = require("async");
 
-const graphhopper = require('./graphhopper_key');
+const graphhopper = require("./graphhopper_key");
 
-var toReturn = "";
+function getLocation(location, callback) {
+    if (location.hasOwnProperty("lat")) {
+        var latLong = encodeURI(location.lat + "," + location.lon);
+        callback(null, latLong);
+    } else {
+        const options = {
+            host: "nominatim.openstreetmap.org",
+            port: 443,
+            path: "/search/" + encodeURI(location) + "?format=json&countrycode=gb",
+            method: "GET",
+            json:true
+        };
 
-exports.handler = (event, context, callback) => {
-	var graphHopper = function (input, callback) {
-	    setTimeout(function() {
-		    var options =     {
-			host: "graphhopper.com",
-			port: 443,
-			method: 'GET',
-			json:true
-		    };
-		    options.path = '/api/1/route?' + input + '&vehicle=bike&key=' + graphhopper.key;
-		    https.request(options, function (res) {
-			res.on('data', function (chunk) {
-			    toReturn = chunk.toString();
-			    // console.log(toReturn);
-			    callback(null, toReturn);
-			});}).end();
-	    },10);
-	};
+        var responseString = "";
+        https.request(options,
+            function(res) {
+                res.on("data", (chunk) => {
+                    responseString += chunk.toString("utf8");
+                });
 
-	var openStreetMap = function (query, callback2) {
-	    setTimeout(function () {
-		    var updateLatLon = function(lat, lon) {
-			var latLon = lat + '%2C' + lon;
-			output = output + "point=" + latLon + "&";
-			if (query.in.length == 1)
-				callback2(null, output);
-			else {
-				var query1 = { in: Array.prototype.slice.call(query.in, 1), out: output };
-				openStreetMap(query1, callback2);
-			}
-		    };
-		    var input = query.in[0];
-		    var output = query.out;
-		    var isStreetName = ! input.lat;
-		    if (!isStreetName) {
-			updateLatLon(input.lat, input.lon);
-		    } else {
-		        var options = {
-			   host: "nominatim.openstreetmap.org",
-			   port: 443,
-			   method: 'GET',
-			   path: '/search/' + encodeURI(input) + '?format=json&countrycode=gb',
-			   json:true
-		        };
-		        https.request(options, function (res) {
-			    res.on('data', function (chunk) {
-			    	var searchResponse = JSON.parse(chunk);
-				updateLatLon(searchResponse[0].lat, searchResponse[0].lon);
-			});}).end();
-		    }
-	    },10);
-	};
-
-	var openStreetMap2 = function(err, data) {
-		// callback(null, data);
-		graphHopper(data, callback);
-	};
-
-  var query = {in: [event.Start, event.End], out: ""};
-
-  openStreetMap(query, openStreetMap2);
+                res.on("end", () => {
+                    var response = JSON.parse(responseString);
+                    var latLong = encodeURI(response[0].lat + "," + response[0].lon);
+                    callback(null, latLong);
+                });
+            }
+        ).end();
+    }
 };
 
+function getRoute(locations, options, callback) {
+    var requestOptions = {
+        host: "graphhopper.com",
+        port: 443,
+        path: "/api/1/route?point=" + locations[0] + "&point=" + locations[1] + "&vehicle=bike&key=" + graphhopper.key,
+        method: "GET",
+        json:true
+    };
+
+    var responseString = "";
+    https.request(requestOptions,
+        function(res) {
+            res.on("data", (chunk) => {
+                responseString += chunk.toString("utf8");
+            });
+
+            res.on("end", () => {
+                var route = responseString.toString("utf8");
+                callback(null, route);
+            });
+        }
+    ).end();
+};
+
+
+exports.handler = (event, context, lambdaCallback) => {
+
+    var err;
+    var results;
+
+    async.parallel([
+        function(callback) {getLocation(event.Start, callback)},
+        function(callback) {getLocation(event.End, callback)}
+        ],
+        function (err, results) {
+            getRoute(results, {}, lambdaCallback);
+        });
+};
